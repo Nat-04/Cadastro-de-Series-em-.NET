7b6fea5fddb4e04e1fe3f20725f89b26c63df6e8
Nat-04 <81697123+Nat-04@users.noreply.github.com>
2021-06-08 18:33:56 -0300

    Add files via upload

---
+++ Series.DIO/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Series.DIO
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get; protected set; }
+    }
+}
---
+++ Series.DIO/Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace Series.DIO
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espinagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
+    }
+}
